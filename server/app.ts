0a02ca8762c1992591b7fa6fb69f01ee2c86b258
Lei Li <lei.xw.li@hotmail.com>
2024-07-10 09:43:22 -0700

    project setup

---
+++ server/app.ts
@@ -0,0 +1,29 @@
+require('dotenv').config();
+import express, {NextFunction, Request, Response} from 'express';
+export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import exp from 'constants';
+
+// body parser
+app.use(express.json({limit: '50mb'}));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors
+app.use(cors({origin: process.env.ORIGIN}));
+
+// testing api
+app.get('/test', (req:Request, res:Response, next:NextFunction) => {
+  res.status(200).json({
+        success: true,
+        message: 'Hello World!'
+    });
+});
+
+app.all("*", (req:Request, res:Response, next:NextFunction) => {
+    const err = new Error(`Route not found - ${req.originalUrl}`) as any;
+    err.statusCode = 404;
+    next(err);
+});
